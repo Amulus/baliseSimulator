3db1aef1080771994f319179477017614a14909c
amine-can <aminechanhih@gmail.com>
2021-01-13 14:24:34 +0100

    Complexite cyclomatique

--- BaliseSimulator/src/graphicLayer/demos/Example4.java
+++ BaliseSimulator/src/graphicLayer/demos/Example4.java
@@ -17,9 +17,8 @@ import graphicLayer.GString;
 
 // I_love_Brest_city
 
-
 public class Example4 {
-	
+
 	public Example4() {
 
 		File path = new File("I_love_Brest_city.jpg");
@@ -29,28 +28,28 @@ public class Example4 {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		GSpace w = new GSpace("Image", new Dimension(rawImage.getWidth() + 10, rawImage.getHeight() + 10));
-		
+
 		GRect container = new GRect();
 		container.withoutBorder();
 		container.setColor(Color.white);
 		container.setDimension(new Dimension(rawImage.getWidth() + 10, rawImage.getHeight() + 10));
 		w.addElement(container);
-		
-		GImage image = new GImage (rawImage);
-		image.setPosition(new Point(5,5));
+
+		GImage image = new GImage(rawImage);
+		image.setPosition(new Point(5, 5));
 		container.addElement(image);
-		
+
 		GString s = new GString();
-		s.setPosition(new Point(10,10));
+		s.setPosition(new Point(10, 10));
 		s.setString("I love Brest...");
 		s.setFont(new Font("Arial", Font.BOLD, 30));
 		s.setColor(Color.red);
 		container.addElement(s);
 		w.open();
 	}
-	
+
 	public static void main(String[] args) {
 		new Example4();
 	}
