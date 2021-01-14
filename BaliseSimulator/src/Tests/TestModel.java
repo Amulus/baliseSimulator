0dead6f8d1a45b8435bb474e81792c1d29e16eaf
amine-can <aminechanhih@gmail.com>
2021-01-14 16:45:50 +0100

    TU manager part

--- BaliseSimulator/src/Tests/TestModel.java
+++ BaliseSimulator/src/Tests/TestModel.java
@@ -10,6 +10,7 @@ import model.DeplHorizontal;
 import model.DeplSatellite;
 import model.DeplSinusoidal;
 import model.DeplVertical;
+import model.Manager;
 import model.Satelitte;
 
 public class TestModel {
@@ -19,41 +20,42 @@ public class TestModel {
 		Balise bal = new Balise(300);
 		bal.setPosition(new Point(400, 200));
 		bal.setDeplacement(new DeplVertical(130, 270));
-		bal.setDeplacement(new DeplSinusoidal(250, 150, 200, 600));
-		bal.setDeplacement(new DeplHorizontal(50, 750));
-		bal.bouge();
-		bal.tick();
-		/*
-		 * System.out.println(bal.getPosition().x+" ,"+bal.getPosition().y);
-		 * System.out.println(bal.memorySize()); System.out.println(bal.profondeur());
-		 * System.out.println(bal.dataSize());
-		 */
 
-		Assert.assertEquals(bal.profondeur(), 200);
-		Assert.assertTrue(bal.getPosition().equals(new Point(412, 200)));
+		for(int i = 0;i<301;i++)
+		{
+			bal.bouge();
+			bal.tick();
+		}
+		
+		  System.out.println(bal.getPosition().x+" ,"+bal.getPosition().y);
+		  System.out.println(bal.memorySize()); 
+		  System.out.println(bal.profondeur());
+		  System.out.println(bal.dataSize());
+		 
+
+		Assert.assertEquals(bal.profondeur(), 266);
+		Assert.assertTrue(bal.getPosition().equals(new Point(400, 266)));
 		Assert.assertTrue(bal.memorySize() == 300);
-		Assert.assertTrue(bal.dataSize() == 1);
+		Assert.assertTrue(bal.dataSize() == 0);
 	}
 
 	@Test
 	public void testBaliseMemoryFull() {
 		Balise bal = new Balise(300);
 		bal.setPosition(new Point(400, 200));
-		bal.setDeplacement(new DeplVertical(130, 270));
-		bal.setDeplacement(new DeplSinusoidal(250, 150, 200, 600));
 		bal.setDeplacement(new DeplHorizontal(50, 750));
 		bal.bouge();
-		bal.tick();
-		/*
-		 * System.out.println(bal.getPosition().x+" ,"+bal.getPosition().y);
-		 * System.out.println(bal.memorySize()); System.out.println(bal.profondeur());
-		 * System.out.println(bal.dataSize());
-		 */
 
-		Assert.assertEquals(bal.profondeur(), 200);
-		Assert.assertTrue(bal.getPosition().equals(new Point(412, 200)));
+		for(int i = 0;i<301;i++)
+		{
+			bal.bouge();
+			bal.tick();
+		}
+
+		Assert.assertEquals(bal.profondeur(), 197);
+		Assert.assertTrue(bal.getPosition().equals(new Point(612, 197)));
 		Assert.assertTrue(bal.memorySize() == 300);
-		Assert.assertTrue(bal.dataSize() == 1);
+		Assert.assertTrue(bal.dataSize() == 0);
 	}
 
 	@Test
@@ -71,6 +73,33 @@ public class TestModel {
 
 		Assert.assertTrue(sat.getPosition().equals(new Point(14, 50)));
 		Assert.assertTrue(sat.memorySize() == 100000);
+	}
+	
+	@Test
+	public void testManager() {
+		Manager manager = new Manager();
+		Balise bal = new Balise(300);
+		bal.setPosition(new Point(400, 200));
+		bal.setDeplacement(new DeplSinusoidal(250, 150, 200, 600));
+		Satelitte sat = new Satelitte(100000);
+		sat.setPosition(new Point(10, 50));
+		sat.setDeplacement(new DeplSatellite(-10, 1000, 2));
+		manager.addBalise(bal);
+		manager.tick();
+		manager.addSatellite(sat);
+		manager.tick();
+		
+//		System.out.println(sat.getPosition().x+" ,"+sat.getPosition().y);
+//		System.out.println(sat.memorySize());
+//		System.out.println("---------");
+//		System.out.println(bal.getPosition().x+" ,"+bal.getPosition().y);
+//		System.out.println(bal.memorySize()); System.out.println(bal.profondeur());
+//		System.out.println(bal.dataSize());
+		Assert.assertTrue(sat.getPosition().equals(new Point(12, 50)));
+		Assert.assertTrue(bal.getPosition().equals(new Point(414, 200)));
+
+		 
+
 	}
 
 }
